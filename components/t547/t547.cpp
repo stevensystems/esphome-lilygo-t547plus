6a8582f19dbff4f62166cb2a8e91b9d2faa9fddd
PrototypeSomething <lucas.backdahl@gmail.com>
2024-10-27 19:37:53 +0100

    changed draw mode on grayscale images

--- components/t547/t547.cpp
+++ components/t547/t547.cpp
@@ -93,7 +93,6 @@ void T547::display_framebuffer() {
 
   epd_poweron();
   epd_draw_grayscale_image(epd_full_screen(), this->buffer_);
-  sleep(50);
   epd_poweroff();
 
   ESP_LOGV(TAG, "Display finished (full) (%ums)", millis() - start_time);
